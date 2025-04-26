93f7de649140ee8fe200ba7929402106b7c4704b
adisudrajat698 <113406705+adisudrajat698@users.noreply.github.com>
2025-04-27 01:56:30 +0700

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const input = req.body.input;
 
   try {
-    const response = await fetch('https://api-inference.huggingface.co/models/google/flan-t5-small', {
+    const response = await fetch('https://api-inference.huggingface.co/models/gpt2', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.HF_API_KEY}`,
